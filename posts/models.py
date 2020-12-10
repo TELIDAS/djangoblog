66c4cfd82fa3861227185f36fc3d9d1a1ad46d46
TELIDAS <71645703+TELIDAS@users.noreply.github.com>
2020-12-10 18:05:56 +0600

    commit

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} {self.description} {self.date_added}'
--- posts/views.py
+++ posts/views.py
@@ -12,11 +12,18 @@ def get_posts(request):
         post = Post.objects.create(title="Good Morning", description="Have a good day")
         return HttpResponse('Put the information to my profile')
     if method_post == 'GET':
-        return HttpResponse('Get the information from my profile')
+        saved_posts = Post.objects.all()
+        return HttpResponse(saved_posts)
     if method_post == 'DELETE':
-        return HttpResponse('Delete my profile')
-    if method_post == 'PATCH':
-        return HttpResponse('New postff patch is available')
+        post = Post.objects.get(pk=1)
+        post.delete()
+        return HttpResponse('Post deleted')
+    if method_post in ['PUT', 'PATCH']:
+        post = Post.objects.create(title='Good Morning', description='Have a good day')
+        post.title = "Good evening"
+        post.description = "Good night"
+        post.save()
+
     # print(vars(HttpResponse))
     return HttpResponse('Hi master, i\'m working!')
 
